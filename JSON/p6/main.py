3d7740a03b0c069c8c19db9ac49b53ed0421a12e
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-10-03 14:08:24 +0330

    this Step tutorial about easy read output program with keyword indent and seperators in output json variable

---
+++ JSON/p6/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import json
+
+x = {
+    "name": "John",
+    "age": 30,
+    "married": True,
+    "divorced": False,
+    "children": ("Ann","Billy"),
+    "pets": None,
+    "cars": [
+        {"model": "BMW 230", "mpg": 27.5},
+        {"model": "Ford Edge", "mpg": 24.1}
+    ]
+}
+
+# use . and a space to seprate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
