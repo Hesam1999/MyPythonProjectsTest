4eb96ad1995a34e0fbfa1b13bfa323fcb07ce4b0
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-11-04 11:57:16 +0330

    Returns a match for any character EXCEPT a, r, and n.

---
+++ RegEx/findall/Sets/p3/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import re
+
+txt = "The rain in Spain"
+
+#Check if the string has other characters than a, r, or n:
+
+x = re.findall("[^arn]", txt)
+
+print(x)
+
+if x :
+    print("Yes, there is at least one match!")
+else:
+    print("No match")
