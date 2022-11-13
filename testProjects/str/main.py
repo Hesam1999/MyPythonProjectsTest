35fdce115e95f5a67ddf66c4a7860c718549690b
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-11-13 21:52:48 +0330

    p7/Returns a match for any character alphabetically between a and z, lower case OR upper case

--- testProjects/str/main.py
+++ testProjects/str/main.py
@@ -1,5 +1,23 @@
-#!/usr/bin/python3
+def count_substring(string, sub_string):
+    n = 0
+    s = 0
+    for x in range(0, len(string)):
+        if sub_string[0] == string[x]:
+            for y in range(x, len(string)):
+                if sub_string[s] == string[y]:
+                    s+=1
+                    if s == len(sub_string):
+                        s = 0
+                        n+=1
+                        break
+                elif s+1 >= len(sub_string):
+                    s = 0
+                    break
+    return n
 
-a = "Hello"
-b = a.split("")
-print(b)
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
