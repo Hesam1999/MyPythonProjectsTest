2b70bc93454b090cae71375b1907fbbcf0b54977
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-11-20 21:00:26 +0330

    PIP is a package manager for Python packages, or modules if you like - Import and use camelcase.

---
+++ pip/p1/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import camelcase
+
+c= camelcase.CamelCase()
+
+txt = "lorem ipsum dolor sit amet"
+
+print(c.hump(txt))
+
+# This method capitalizes the first letter of each word.
