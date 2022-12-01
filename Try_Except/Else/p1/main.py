bacf7fecf1f7fac4bd844e1d7075ad83924a9437
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-12-02 00:29:38 +0330

    Python Try Except - In this example, the try block does not generate any error

---
+++ Try_Except/Else/p1/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# The try block does not raise any errors, so the else block is executed:
+
+try:
+    print("Hello")
+except:
+    print("Somthing else went wrong")
+else:
+    print("Nothing went wrong")
+
