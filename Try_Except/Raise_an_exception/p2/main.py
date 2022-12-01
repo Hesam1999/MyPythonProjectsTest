a449d7b65bd1252d051a816a428b0944b2c5ab57
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-12-02 00:23:27 +0330

    Python Try Except - Raise a TypeError if x is not an integer

---
+++ Try_Except/Raise_an_exception/p2/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+x = "hello"
+
+if not type(x) is int:
+    raise TypeError("Only integers are allowed")
--- Try_Except/p2/main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-# This will rales an exception, because x is not defined:
-
-    print(x)
