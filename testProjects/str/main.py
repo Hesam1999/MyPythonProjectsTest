f4fb8d00810d8546fe2b3a61bfa613f665afa6be
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-12-02 19:40:07 +0330

    Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications - Mat size must be NxM.N ( is an odd natural number, and M is 3 times N.) - The design should have 'WELCOME' written in the center - The design pattern should only use |, . and - characters.

--- testProjects/str/main.py
+++ testProjects/str/main.py
@@ -1,19 +1,71 @@
-import textwrap
 
-def wrap(string, max_width):
-    li = []
-    s = 0
-    for x in string:
-        if s == max_width:
-            li.append("\n")
-            li.append(x)
-            s = 0
-        elif s < max_width and s >= 0:
-            li.append(x)
-            s+=1
-    return "".join(li)
+N, M = map(int, input().split())
 
-if __name__ == '__main__':
-    string, max_width = input(), int(input())
-    result = wrap(string, max_width)
-    print(result)
+mat = ""
+
+s = (M//2)+1
+lsz = 0
+sz = 0
+lz = 0
+z = 1
+lzx = 0
+zx = -1
+i = 0
+x = 0
+u = 0
+while i < N:
+    while x < M:
+        u = 1
+        if i < (N//2):
+            # create line
+            if x == ((M // 2) - (1 + z + zx)):
+                #-------------------------
+                lz = z
+                lzx = zx
+                lsz = sz
+                #-------------------------
+                for c in range(0, z):
+                    mat += ".|."
+
+                x = (M // 2) + z + sz
+                
+                           
+            else : 
+                mat += "-"
+            
+        elif i == (N//2):
+            u = 0
+            if x < ((M - 6) // 2):
+                mat += "-"
+            elif x == ((M - 6) // 2):
+                q = "WELCOME"
+                mat += q
+                x += len(q) -1
+                # ----------------------------
+                z = lz
+                zx = lzx
+                sz = lsz
+            else:
+                mat += "-"
+        elif i > (N//2):
+            u = 2
+            if x == ((M // 2) - (1 + z + zx)):
+                for c in range(0, z):
+                    mat += ".|."
+
+                x = (M // 2) + z + sz
+            elif (x < ((M // 2) - (1 + z + zx))) or (x > (M // 2) + z + sz):
+                mat += "-"
+        x += 1
+    mat += "\n"
+    i += 1
+    x = 0 
+    if u == 1:
+        z += 2
+        zx += 1
+        sz += 1
+    elif u == 2:
+        z -= 2
+        zx -= 1
+        sz -= 1
+print(mat)
